d7cc02b2e9db59222df8d6a2d2337bbe93b278a1
gitapin82y <gitapin82y@gmail.com>
2024-01-16 23:08:16 +0700

    final project

--- resources/views/pages/reseller/ecommerce-information.blade.php
+++ resources/views/pages/reseller/ecommerce-information.blade.php
@@ -45,7 +45,7 @@
                                     </div>
                                     <div class="form-group">
                                         <label for="name" class="form-control-label">Shop Name</label><br>
-                                        <input type="text" id="name" class="form-control" value="{{optional($ecommerce)->name ? $ecommerce->name : ''}}" name="name" required>
+                                        <input type="text" id="name" class="form-control" value="{{optional($ecommerce)->name ? $ecommerce->name : 'Esellexpress'}}" name="name" required>
                                     </div>
                                     <div class="form-group">
                                         <label for="logo" class="form-control-label">Logo</label>
