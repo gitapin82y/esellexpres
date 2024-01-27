4f2fd963eaff6d4c6b3e3df4392b21291c814c53
gitapin82y <gitapin82y@gmail.com>
2024-01-27 19:58:55 +0700

    add last activity and middleware

---
+++ app/Http/Middleware/CheckUserActivity.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckUserActivity
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth()->check()) {
+            $user = auth()->user();
+            $lastActivity = $user->last_activity;
+    
+            if ($lastActivity && now()->diffInDays($lastActivity) >= 2) {
+                auth()->logout();
+                return redirect('/login')->with('status', 'You have been automatically logged out due to no activity for 2 days.');
+            }
+    
+            // Update waktu terakhir aktivitas
+            $user->update(['last_activity' => now()]);
+        }
+    
+        return $next($request);
+    }
+}
